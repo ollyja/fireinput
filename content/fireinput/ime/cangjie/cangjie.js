ec6d915f0afb58f51acba04aea1044e504991544
ollyja <ollyja@666eac4c-442f-0410-ab37-87f3f73558d0>
2008-05-14 22:23:21 +0000

    correct wrong comment

--- content/fireinput/ime/cangjie/cangjie.js
+++ content/fireinput/ime/cangjie/cangjie.js
@@ -358,7 +358,7 @@ Cangjie.prototype = extend(new FireinputIME(),
        if (! this.keyCangjieHash.hasItem(keyInitial))
        	  return null;
 
-       // only enable autoinsertion for 4 keys
+       // only enable autoinsertion for 5 keys
        if (key.length >= 5)
        	this.autoInsertion = true;
 
